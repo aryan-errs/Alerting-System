171b917e859ba3495448b9af82b4255956c29b0a
Aryan Sharma <aryan182282@gmail.com>
2025-01-09 09:36:37 +0530

    feat:added email alert

--- src/models/FailedRequest.ts
+++ src/models/FailedRequest.ts
@@ -5,7 +5,10 @@ const FailedRequestSchema = new mongoose.Schema({
   timestamp: { type: Date, default: Date.now, index: true },
   reason: { type: String, required: true },
   headers: { type: Object },
-  endpoint: { type: String, required: true },
+  endpoint: { type: String, required: true }
+}, {
+  // Add TTL index to automatically remove old records
+  expires: 60 * 60 * 24 * 30 // 30 days
 });
 
 export const FailedRequest = mongoose.model('FailedRequest', FailedRequestSchema);
